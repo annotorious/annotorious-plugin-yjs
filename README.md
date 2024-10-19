a8dbb03830aea147a68064a7c6de066e7cf0dfb4
Rainer Simon <rainer@rainersimon.io>
2024-10-19 15:14:39 +0200

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# annotorious-yjs
-YJS-backed storage for Annotorious
+# Annotorious YJS
+
+YJS-backed storage for Annotorious.
+
+## Start Server
+
+```
+HOST=localhost PORT=1234 npx y-websocket
+```
+
+
--- src/mountPlugin.ts
+++ src/mountPlugin.ts
@@ -1,20 +1,22 @@
 import * as Y from 'yjs';
-import { Origin, type Annotator } from '@annotorious/core';
+import { Origin, type Annotation, type Annotator } from '@annotorious/core';
 
 export const mountPlugin = (anno: Annotator) => {
 
   const { store } = anno.state;
 
-  const ydoc = new Y.Doc();
+  const ydoc = new Y.Doc()
 
-  const ymap = ydoc.getMap('annotations');
+  const ymap = ydoc.getMap<Annotation>('annotations');
 
   ymap.observe(event => {
+    if (event.transaction.local) return;
+    
     event.changes.keys.forEach((change, key) => {
-      if (change.action === 'add' || change.action === 'update') {
-        const yAnnotation = ymap.get(key);
-        const annotation = yAnnotationToAnnotation(yAnnotation);
-        store.updateAnnotation(annotation, Origin.REMOTE);
+      if (change.action === 'add') {
+        store.addAnnotation(ymap.get(key), Origin.REMOTE);
+      } else if (change.action === 'update') {
+        store.updateAnnotation(ymap.get(key), Origin.REMOTE);
       } else if (change.action === 'delete') {
         store.deleteAnnotation(key, Origin.REMOTE);
       }
@@ -24,48 +26,25 @@ export const mountPlugin = (anno: Annotator) => {
   store.observe(event => {
     if (event.origin === Origin.LOCAL) {
       event.changes.created?.forEach(annotation => {
-        const yAnnotation = annotationToYAnnotation(annotation);
-        ymap.set(annotation.id, yAnnotation);
+        ymap.set(annotation.id, annotation);
       });
 
       event.changes.updated?.forEach(update => {
-        const yAnnotation = annotationToYAnnotation(update.newValue);
-        ymap.set(update.newValue.id, yAnnotation);
+        ymap.set(update.newValue.id, update.newValue);
       });
 
       event.changes.deleted?.forEach(annotation => {
         ymap.delete(annotation.id);
       });
     }
-  });
+  }, { origin: Origin.LOCAL });
 
-  function yAnnotationToAnnotation(yAnnotation) {
-    return {
-      id: yAnnotation.get('id'),
-      bodies: yAnnotation.get('bodies').toArray(),
-      target: yAnnotation.get('target').toJSON()
-    };
-  }
-
-  function annotationToYAnnotation(annotation) {
-    const yAnnotation = new Y.Map();
-    yAnnotation.set('id', annotation.id);
-    yAnnotation.set('bodies', new Y.Array());
-
-    // @ts-ignore
-    yAnnotation.get('bodies').push(annotation.bodies);
-
-    yAnnotation.set('target', new Y.Map(Object.entries(annotation.target)));
-    return yAnnotation;
-  }
-
-  // Initialize the store with data from YJS
-  ymap.forEach((yAnnotation, key) => {
-    const annotation = yAnnotationToAnnotation(yAnnotation);
+  ymap.forEach((annotation, key) => {
     store.addAnnotation(annotation, Origin.REMOTE);
   });
 
   return {
     ydoc
-  };
+  }
+
 }
